e82fd9acb740db34d1bda5e6aabd1dc058a25612
ICI-INC <166365607+ICI-INC@users.noreply.github.com>
2025-02-27 03:41:51 -0300

    Add files via upload

---
+++ cliente/Yasmindiniz/dados/dados.js
@@ -0,0 +1,13 @@
+function logar(){
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "Yasmin" && senha == "1998"){ //yasmin
+        alert('Sucesso');
+        location.href = "https://ici-inc.github.io/ASF/3/index.html";
+
+    }
+            
+}
+
